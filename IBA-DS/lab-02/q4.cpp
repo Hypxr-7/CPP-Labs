674a6752d2535340116202e93384b02f6a09d98d
Hypxr-7 <Hypxr-7@users.noreply.github.com>
2024-09-02 22:19:29 +0500

    Add IBA-DS/lab-02/q4.pdf and Edit IBA-DS/lab-02/q4.cpp
    Fixed q4.cpp that using algorithm with a O(n^2) complexity instead of the required O(n^3).
    Also changed code functionality ti display individual algorithm time.

--- IBA-DS/lab-02/q4.cpp
+++ IBA-DS/lab-02/q4.cpp
@@ -1,47 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <random>
-#include <set>
 #include <algorithm>
 
+constexpr int N = 400;
 
-constexpr int N = 10000;
-
-void myFunc();
+void linear(std::vector<int>& vec);
+void linearLog(std::vector<int>& vec);
+void cube(std::vector<int>& vec);
+void callAllFunctions(std::vector<int>& vec);
 
 int main() {
-    clock_t start = clock();
-    myFunc();
-    clock_t end = clock();
+    std::vector<int> vec;
 
-    double time = double(end - start) / double(CLOCKS_PER_SEC);
-    std::cout << "Time Taken: " << time << " seconds\n";
+    callAllFunctions(vec);
 
     return 0;
 }
 
-void myFunc() {
+void linear(std::vector<int>& vec) {
+    // O(n)
+    // initializes the array  
+
     std::mt19937 mt(std::random_device{}());
     std::uniform_int_distribution<int> dist(-50, 50);
 
-    // O(n)
-    // initializes the array  
-    std::vector<int> vec;
+    auto start = clock();
     for (int i = 0; i < N; ++i) 
         vec.push_back(dist(mt));
+    auto end = clock();
+    double time = double(end - start) / double(CLOCKS_PER_SEC);
+    std::cout << "Time Taken By Linear: " << time << " seconds\n";
+}
 
-    // O(n²)
-    // makes a list of all possible pair permutations
-    std::set<std::pair<int, int>> pairs;
-    for (int i = 0; i < N; ++i) { 
-        for (int j = 0; j < N; ++j) {
-            if (i == j) continue;
-            pairs.insert(std::pair<int, int>(vec[i], vec[j])); 
-        }
-    }
-
+void linearLog(std::vector<int>& vec) {
     // O(n·log(n))
     // sorting the array 
     // complexity verified by https://en.cppreference.com/w/cpp/algorithm/sort#Complexity
+    auto start = clock();
     std::sort(vec.begin(), vec.end());
+    auto end = clock();
+    double time = double(end - start) / double(CLOCKS_PER_SEC);
+    std::cout << "Time Taken By Linear-Log: " << time << " seconds\n";
+}
+
+void cube(std::vector<int>& vec) {
+    // O(n³)
+    auto start = clock();
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            for (int k = 0; k < N; ++k)
+                if (vec[i] == vec[j] && vec[j] == vec[k]); // TODO: do something
+    auto end = clock();
+    double time = double(end - start) / double(CLOCKS_PER_SEC);
+    std::cout << "Time Taken By Cubic: " << time << " seconds\n";
+}
+
+void callAllFunctions(std::vector<int>& vec) {
+    auto start = clock();
+    linear(vec);
+    linearLog(vec);
+    cube(vec);
+    auto end = clock();
+    double time = double(end - start) / double(CLOCKS_PER_SEC);
+    std::cout << "Time Taken By All: " << time << " seconds\n";
 }
